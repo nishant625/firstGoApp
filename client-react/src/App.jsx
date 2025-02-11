619125a9f18c7d410208ac997a59e35ad7092f40
nishant625 <gangwarnishant625@gmail.com>
2025-02-11 18:44:08 +0530

    added a workflow first

--- client-react/src/App.jsx
+++ client-react/src/App.jsx
@@ -3,10 +3,12 @@ import {
   QueryClientProvider,
   useQuery,
 } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import axios from "axios";
+import Clock from 'react-clock';  // Import the react-clock package
+import 'react-clock/dist/Clock.css';  // Import the default clock styles
 
-import './App.css'
+import './App.css';
 
 const queryClient = new QueryClient();
 
@@ -19,29 +21,38 @@ function CurrentTime(props) {
         .then((res) => res.data),
   });
 
-  if (isLoading) return `Loading ${props.api}... `;
+  if (isLoading) return `Loading ${props.api}...`;
 
   if (error) return "An error has occurred: " + error.message;
+
+  // Convert fetched time to Date object
+  const fetchedTime = new Date(data.now);
 
   return (
     <div className="App">
       <p>---</p>
       <p>API: {data.api}</p>
-      <p>Time from DB: {data.now}</p>
+      <p>Time from DB:</p>
+      {/* Display the fetched time in a clock */}
+      <Clock 
+        value={fetchedTime} 
+        renderNumbers={true} 
+        className="custom-clock" 
+      />
       <div>{isFetching ? "Updating..." : ""}</div>
     </div>
-  )
+  );
 }
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>Hey Team! 👋</h1>
-      <CurrentTime api="/api/golang/"/>
-      <CurrentTime api="/api/node/"/>
+      <h1>This is my first golang api with react js as frontend</h1>
+      <CurrentTime api="/api/golang/" />
+      <CurrentTime api="/api/node/" />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
 
-export default App
+export default App;
